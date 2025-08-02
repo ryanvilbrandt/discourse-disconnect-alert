190bf05a5a8d8f9d00fd1f889f693acea357b27c
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2025-08-01 20:22:42 -0700

    Version 1.0

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-disconnect-alert
 # about: Alerts users with a banner if the server cannot be reached
-# version: 0.1
+# version: 1.0
 # authors: Ryan Vilbrandt
 
 enabled_site_setting :disconnect_alert_enabled
